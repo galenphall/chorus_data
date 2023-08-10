c8e58572dc78449263264f6a1dcd8fd50ec912c2
Galen Hall <galen_hall@brown.edu>
2023-08-10 10:36:36 -0400

    string block assignment values

--- code/main.py
+++ code/main.py
@@ -49,6 +49,7 @@ def main():
             )) for level in range(len(blockstate.levels))}
             blocks_df = pd.DataFrame(blocks)
             blocks_df.columns = 'block_' + blocks_df.columns.astype(str)
+            blocks_df = blocks_df.applymap(lambda x: f"{state}_{record_type}_{int(str(x))}")
             blocks_df['state'] = state
             blocks_df['record_type'] = record_type
             blocks_df.index.name = 'entity_id'
